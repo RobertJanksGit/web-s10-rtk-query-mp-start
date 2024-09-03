8b720353331b5c9a5d72f41dabe036cfa3cddc69
Robert Jank <robert.jank@yahoo.com>
2024-09-03 10:03:01 -0500

    wired RTK Query to application and implemented getQuotes query. Started work on implementing the mutations.

--- frontend/state/quotesApi.js
+++ frontend/state/quotesApi.js
@@ -1,1 +1,43 @@
-// create your RTK Query endpoints here
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const quoteApi = createApi({
+  reducerPath: "quoteApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:9009/api/" }),
+  tagTypes: ["Quotes"],
+  endpoints: (build) => ({
+    getQuotes: build.query({
+      query: () => "quotes",
+      providesTags: ["Quotes"],
+    }),
+    createQuote: build.mutation({
+      query: (quote) => ({
+        url: "quotes",
+        method: "POST",
+        body: quote,
+      }),
+      invalidatesTags: ["Quotes"],
+    }),
+    toggleFake: build.mutation({
+      query: ({ id, quote }) => ({
+        url: `quotes/${id}`,
+        method: "PUT",
+        body: quote,
+      }),
+      invalidatesTags: ["Quotes"],
+    }),
+    deleteQuote: build.mutation({
+      query: (id) => ({
+        url: `quotes/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["Quotes"],
+    }),
+  }),
+});
+
+export const {
+  useGetQuotesQuery,
+  useCreateQuoteMutation,
+  useToggleFakeMutation,
+  useDeleteQuoteMutation,
+} = quoteApi;
--- frontend/state/store.js
+++ frontend/state/store.js
@@ -1,8 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit'
-import quotesReducer from './quotesSlice'
+import { configureStore } from "@reduxjs/toolkit";
+import quotesReducer from "./quotesSlice";
+import { quoteApi } from "./quotesApi";
 
 export const store = configureStore({
   reducer: {
     quotesState: quotesReducer,
-  }
-})
+    [quoteApi.reducerPath]: quoteApi.reducer,
+  },
+  middleware: (getDefault) => getDefault().concat(quoteApi.middleware),
+});
